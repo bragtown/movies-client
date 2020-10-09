1a5cb9b48d4d998829f77ee15c18998a4325b4d5
lostscrolls <lostscrolls@churchofjesuschrist.org>
2020-10-08 18:09:22 -0600

    add bootstrap and react router dom to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar bg="light">
+        <Navbar.Brand>Bragtown Movies</Navbar.Brand>
+
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Movies</Nav.Link>
+          <Nav.Link href="/new">Add Movie</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Switch>
+        <Route path="/" exact>
+          {/*add movie component*/}
+        </Route>
+        <Route path="/new" exact>
+          {/*add movie input component*/}
+        </Route>
+        <Route path="/movie/:id">
+          {/*add movie view*/}
+        </Route>
+
+
+      </Switch>
+    </Router>
   );
 }
 
